929ca433d29e0a639df2add738f63ba60add6cf0
KrupskiiAndrei <117271890+KrupskiiAndrei@users.noreply.github.com>
2023-05-11 20:11:59 +0300

    Update model_testing.py

--- lab1/model_testing.py
+++ lab1/model_testing.py
@@ -2,10 +2,10 @@ import pandas as pd
 import pickle
 from sklearn.metrics import accuracy_score
 
-with open('lab1/model.pkl', 'rb') as f:
+with open('model.pkl', 'rb') as f:
     model = pickle.load(f)
 
-df_test = pd.read_csv('lab1/test/data_scaled.csv')
+df_test = pd.read_csv('test/data_scaled.csv')
 X_test = df_test.drop('target', axis=1)
 y_test = df_test['target']
 
