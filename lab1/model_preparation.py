7d6513e9cb0218ed1bf48d08b6cd4c88053ffa71
KrupskiiAndrei <117271890+KrupskiiAndrei@users.noreply.github.com>
2023-05-11 20:11:34 +0300

    Update model_preparation.py

--- lab1/model_preparation.py
+++ lab1/model_preparation.py
@@ -2,12 +2,12 @@ from sklearn.linear_model import LogisticRegression
 import pandas as pd
 import pickle
 
-df_train = pd.read_csv('lab1/train/data_scaled.csv')
+df_train = pd.read_csv('train/data_scaled.csv')
 X_train = df_train.drop('target', axis=1)
 y_train = df_train['target']
 
 model = LogisticRegression()
 model.fit(X_train, y_train)
 
-with open('lab1/model.pkl', 'wb') as f:
+with open('model.pkl', 'wb') as f:
     pickle.dump(model, f)
