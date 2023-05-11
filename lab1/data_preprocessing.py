196e0048c19fe2c75149f30fd748633814bc8a95
KrupskiiAndrei <117271890+KrupskiiAndrei@users.noreply.github.com>
2023-05-11 20:10:59 +0300

    Update data_preprocessing.py

--- lab1/data_preprocessing.py
+++ lab1/data_preprocessing.py
@@ -3,20 +3,20 @@ import pandas as pd
 
 scaler = StandardScaler()
 
-df_train = pd.read_csv('lab1/train/data.csv')
+df_train = pd.read_csv('train/data.csv')
 X_train = df_train.drop('target', axis=1)
 y_train = df_train['target']
 X_train_scaled = scaler.fit_transform(X_train)
 
-df_test = pd.read_csv('lab1/test/data.csv')
+df_test = pd.read_csv('test/data.csv')
 X_test = df_test.drop('target', axis=1)
 y_test = df_test['target']
 X_test_scaled = scaler.transform(X_test)
 
 df_train_scaled = pd.DataFrame(data=X_train_scaled)
 df_train_scaled['target'] = y_train
-df_train_scaled.to_csv('lab1/train/data_scaled.csv', index=False)
+df_train_scaled.to_csv('train/data_scaled.csv', index=False)
 
 df_test_scaled = pd.DataFrame(data=X_test_scaled)
 df_test_scaled['target'] = y_test
-df_test_scaled.to_csv('lab1/test/data_scaled.csv', index=False)
+df_test_scaled.to_csv('test/data_scaled.csv', index=False)
