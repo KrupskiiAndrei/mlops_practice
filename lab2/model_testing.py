6f7832a8a62256d2cf077440cb81b04e3e9b64c1
KrupskiiAndrei <117271890+KrupskiiAndrei@users.noreply.github.com>
2023-06-06 22:20:29 +0300

    Update model_testing.py

--- lab2/model_testing.py
+++ lab2/model_testing.py
@@ -7,14 +7,14 @@ with open("trained_model.pkl", "rb") as f:
     model = pickle.load(f)
 
 # Загрузка данных для тестирования модели
-х_test = pd.read_csv("test/x_test.csv")
+х_test = pd.read_csv("test/х_test.csv")
 y_test = pd.read_csv("test/y_test.csv")
 
-# Предсказание на тестовых данных
-y_pred = model.predict(X_test)
+# Прогнозирование на тестовых данных
+y_pred = model.predict(х_test)
 
 # Оценка качества модели
 mse = mean_squared_error(y_test, y_pred)
 
 # Вывод результатов
-print("Mean squared error: %.2f" % mse)
+print(f"Среднеквадратичная ошибка: {mse:.2f}")
