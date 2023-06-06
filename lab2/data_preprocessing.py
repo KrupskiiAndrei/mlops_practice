ee23074dba8f0106bd1f90fd4dfd627e943202dd
KrupskiiAndrei <117271890+KrupskiiAndrei@users.noreply.github.com>
2023-06-06 22:18:58 +0300

    Update data_preprocessing.py

--- lab2/data_preprocessing.py
+++ lab2/data_preprocessing.py
@@ -3,21 +3,21 @@ import pandas as pd
 
 # Загрузка данных для обучающей и тестовой выборки
 train_data = pd.read_csv("train/train_data.csv")
-test_data = pd.read_csv("test/x_test.csv")
+test_data = pd.read_csv("test/test_data.csv")
 
 # Выделение признаков и меток
-X_train = train_data.drop(columns=["Date"])  # Удаление столбца "Date" как признака
-y_train = train_data['Temperature']  # Использование "Temperature" как целевой переменной
-X_test = test_data.drop(columns=["Date"])
-y_test = test_data['Temperature']
+х_train = train_data.drop(columns=["Date"])
+y_train = train_data['Cost']
+х_test = test_data.drop(columns=["Date"])
+y_test = test_data['Cost']
 
 # Применение предобработки данных с использованием StandardScaler
 scaler = StandardScaler()
-X_train = scaler.fit_transform(X_train)
-X_test = scaler.transform(X_test)
+х_train = scaler.fit_transform(х_train)
+х_test = scaler.transform(х_test)
 
-# Сохранение предобработанных данных в CSV-файлы
-pd.DataFrame(X_train).to_csv('train/x_train.csv', index=False)
+# Сохранение предобработанных данных
+pd.DataFrame(X_train).to_csv('train/X_train.csv', index=False)
 pd.DataFrame(y_train).to_csv('train/y_train.csv', index=False)
-pd.DataFrame(X_test).to_csv('test/x_test.csv', index=False)
+pd.DataFrame(х_test).to_csv('test/х_test.csv', index=False)
 pd.DataFrame(y_test).to_csv('test/y_test.csv', index=False)
